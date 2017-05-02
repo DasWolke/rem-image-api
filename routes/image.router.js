56161cdbd934d71c621c8ca1e83fd8fac7908ded
DasWolke <julianhannig@gmail.com>
2017-05-02 21:35:42 +0200

    removed config ^^

--- routes/image.router.js
+++ routes/image.router.js
@@ -3,8 +3,7 @@
  */
 const version = require('../package.json').version;
 let router = require('express').Router();
-router.get('/', ((req, res) => {
+router.all('/', ((req, res) => {
     return res.status(200).json({version, message: 'Welcome to the rem-image-api'})
 }));
-
 module.exports = router;
